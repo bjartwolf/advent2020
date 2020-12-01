c46e4067993abe9ae8a52ac5726e51ab20d7ef33
bjartwolf <bjartnes@gmail.com>
2020-12-01 10:19:30 +0100

    better

--- 1.1/calculate.fsx
+++ 1.1/calculate.fsx
@@ -1,16 +1,19 @@
 open System.IO
 
-let numbers = File.ReadLines("input.txt") |> Seq.map int |> Seq.toList
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
 
 let crossproduct l1 l2 l3 =
   seq { for el1 in l1 do
           for el2 in l2 do
             for el3 in l3 do
-                yield el1, el2, el3 }
+                yield int el1, int el2, int el3 }
 
-let numberPairs = crossproduct numbers numbers numbers
-
-let twentytwenty = numberPairs 
+let numbers = readLines "input.txt" 
+let twentytwenty = crossproduct numbers numbers numbers
                     |> Seq.filter (fun (x,y,z) -> x + y + z = 2020) 
                     |> Seq.take 1
                     |> Seq.map (fun (x,y,z) -> (x,y,z,x+y+z,x*y*z))
