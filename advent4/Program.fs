5755e59e7cea5f140f9a03ce9318fe8f80ca6c20
bjartwolf <bjartnes@gmail.com>
2020-12-04 16:21:39 +0100

    nr 4

--- advent4/Program.fs
+++ advent4/Program.fs
@@ -9,15 +9,9 @@ module TryParser =
         | true, v    -> Some v
         | false, _   -> None
 
-    let parseDate   = tryParseWith System.DateTime.TryParse
     let parseInt    = tryParseWith System.Int32.TryParse
-    let parseSingle = tryParseWith System.Single.TryParse
-    let parseDouble = tryParseWith System.Double.TryParse
 
-    let (|Date|_|)   = parseDate
     let (|Int|_|)    = parseInt
-    let (|Single|_|) = parseSingle
-    let (|Double|_|) = parseDouble
 
 module PassportData =
     open TryParser
