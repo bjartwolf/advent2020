fd2a025b539cc427ab98a8a7a2a07c21bcaff787
bjartwolf <bjartnes@gmail.com>
2020-12-07 21:05:06 +0100

    works with lots of fan

--- advent7/run2.sh
+++ advent7/run2.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env fish
 function findbags 
 #  set bags (cat foo.txt | grep $argv' bags\?'| sed 's/ bags.*//g')
-  echo 'called with '$argv'x'
-  set bagsInbag (cat foo.txt | grep '^'$argv' bags\?'| grep -o '[1-9][a-z ]*bag' | sed 's/ bag.*//g')
+  set bagsInbag (cat input7.txt | grep '^'$argv' bags\?'| grep -o '[1-9][a-z ]*bag' | sed 's/ bag.*//g')
   for bagAndCount in $bagsInbag
     set nrOfBags (echo $bagAndCount | grep '[1-9]' -o)
     set bag (echo $bagAndCount | sed 's/[1-9] //g')
-    echo 'nrofbags: '$nrOfBags
-    echo 'bag: '$bag
-    findbags $bag
+#    echo 'nrofbags: '$nrOfBags
+#    echo 'bag: '$bag
+    for i in (seq $nrOfBags)
+      echo $i$bag
+      findbags $bag
+    end
   end
 end
 
