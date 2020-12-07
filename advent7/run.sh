be946fa756fa54c7c32b2317d8a1186e5cde995f
bjartwolf <bjartnes@gmail.com>
2020-12-07 19:21:38 +0100

    finally

--- advent7/run.sh
+++ advent7/run.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env fish
-set sum 0
 function findbags 
-	for bag in $argv
-		set regex $bag' bags[.,]'
-		set nr (cat input7.txt | grep $regex | wc -l)
-		cat input7.txt | grep $regex 
-		set sum (math $nr + $sum)
-		set indirect_bags (cat input7.txt | grep $regex | sed 's/ bags.*//g')
-		if [ -n "$indirect_bags" ]
-			echo $indirect_bags
-			findbags $indirect_bags
-		end
+	set bag $argv
+	set regex $bag' bags\?[.,]'
+	set contained_bags (cat input7.txt | grep $regex | sed 's/ bags.*//g')
+	for currentBag in $contained_bags
+		echo $currentBag
+	end
+	for contained_bag in $contained_bags
+		findbags $contained_bag
 	end
 end
 
-findbags "shiny gold" 
-echo $sum 
+echo 'Sum is '(findbags "shiny gold" | sort | uniq | wc -l)
+
+
+
