5cf25884fe72fd4a1e08f020b83a25f9512935c9
bjartwolf <bjartnes@gmail.com>
2020-12-07 09:44:15 +0100

    not yet recursive

--- advent7/run.sh
+++ advent7/run.sh
@@ -3,18 +3,25 @@
 
 set sum (math (cat input7.txt | grep 'shiny gold bags[,.]'  | wc -l))
 echo "bags that can hold directly " $sum
-
 cat input7.txt | grep 'shiny gold bags[,.]' 
 
 # bags that can hold directly
 echo "bags that can hold in indirectly in one step"
-set indirect_bags (cat input7.txt | grep 'shiny gold bags[,.]' | sed 's/bags.*//g')
-for bag in $indirect_bags;
-	echo "bag : " $bag
-	set regex $bag'bags[,.]' 
-	cat input7.txt | grep $regex 
-	set nr (cat input7.txt | grep $regex | wc -l)
-	set sum (math $nr + $sum) 
+function findbags 
+	set bags $argv
+	for bag in $indirect_bags;
+		echo "****"
+		echo "bag : " $bag
+		set regex $bag'bags[,.]' 
+		cat input7.txt | grep $regex 
+		set nr (cat input7.txt | grep $regex | wc -l)
+		set sum (math $nr + $sum) 
+
+		# continue in the tree
+		# set indirect_bags (cat input7.txt | grep 'shiny gold bags[,.]' | sed 's/bags.*//g')
+	end
 end
 
+set indirect_bags (cat input7.txt | grep 'shiny gold bags[,.]' | sed 's/bags.*//g')
+findbags $indirect_bags
 echo $sum
