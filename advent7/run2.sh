011783576fc7304a73917b082cee93c799affa24
bjartwolf <bjartnes@gmail.com>
2020-12-07 21:06:40 +0100

    works with lots of fan

--- advent7/run2.sh
+++ advent7/run2.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env fish
 function findbags 
-#  set bags (cat foo.txt | grep $argv' bags\?'| sed 's/ bags.*//g')
   set bagsInbag (cat input7.txt | grep '^'$argv' bags\?'| grep -o '[1-9][a-z ]*bag' | sed 's/ bag.*//g')
   for bagAndCount in $bagsInbag
     set nrOfBags (echo $bagAndCount | grep '[1-9]' -o)
     set bag (echo $bagAndCount | sed 's/[1-9] //g')
-#    echo 'nrofbags: '$nrOfBags
-#    echo 'bag: '$bag
     for i in (seq $nrOfBags)
       echo $i$bag
       findbags $bag
@@ -14,5 +11,4 @@ function findbags
   end
 end
 
-#echo 'Sum is '(findbags "shiny gold" | sort | uniq | wc -l)
-findbags "shiny gold"
+echo 'Sum is '(findbags "shiny gold" | wc -l)
