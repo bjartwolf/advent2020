cb8c81cda5f8dc4d651e794a814853912be45df7
bjartwolf <bjartnes@gmail.com>
2020-12-07 20:20:58 +0100

    finally

--- advent7/run.sh
+++ advent7/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env fish
 function findbags 
-  for contained_bag in (cat input7.txt | grep $argv' bags\?[.,]'| sed 's/ bags.*//g')
-    echo $contained_bag
-    findbags $contained_bag
-  end
+  for bag in (cat input7.txt | grep $argv' bags\?[.,]'| sed 's/ bags.*//g'); echo $bag; findbags $bag; end
 end
 
 echo 'Sum is '(findbags "shiny gold" | sort | uniq | wc -l)
