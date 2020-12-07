ea81cf9f474421fb3a72980b9bea828adfb927ed
bjartwolf <bjartnes@gmail.com>
2020-12-07 15:26:03 +0100

    foo

--- advent7/backup.sh
+++ advent7/backup.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env fish
 function findbags 
+	echo '\n'
 	set bag $argv
 	set regex $bag' bags[.,]'
-	set indirect_bags (cat input7.txt | grep $regex | sed 's/ bags.*//g')
-	if [ -n "$indirect_bags" ]
-		for newbag in $indirect_bags
-			set regex $bag' bags[.,]'
-			set count (cat input7.txt | grep '[0-9] '$bag -o | grep '[0-9]' -o)
-#			echo (cat input7.txt | grep $regex) 
-			for i in $count
-				echo 'Bag is at: '$bag
-				findbags $newbag
-			end
-		end
-	else 
-		# do something if not
+	set contained_bags (cat input7.txt | grep $regex | sed 's/ bags.*//g')
+	for contained_bag in $contained_bags
+		echo 'bag is '$bag' the one we look at is '$contained_bag
+#		cat input7.txt | grep '^'$bag | grep '[0-9] ' #$newbag -o | grep '[0-9]' -o
+#		cat input7.txt | grep '^'$regex | grep '[0-9] '$bag -o | grep '[0-9]' -o
+#		cat input7.txt | grep $regex | sed 's/ bags.*//g'
+#		cat input7.txt |grep '^'$newbag 
+#		echo $count
+#		set count (cat input7.txt |grep '^'$bag | grep $newbagregex -o | grep '[0-9]' -o)
+#		echo $count
+#		for i in (seq $count)
+		findbags $contained_bag
+#		end
 	end
 end
 
