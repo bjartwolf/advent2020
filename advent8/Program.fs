8f90e6e6361695acf5c107405350dd172f2b0c09
bjartwolf <bjartnes@gmail.com>
2020-12-08 10:21:39 +0100

    init

---
+++ advent8/Program.fs
@@ -0,0 +1,58 @@
+﻿open System
+open Xunit
+open System.IO
+
+let readInputfile inputfile : string[] =
+    File.ReadAllLines inputfile
+
+type Cmd = Nop | Jmp of int | Acc of int
+
+let parseLine (line: string) : Cmd =
+    if line.Contains("acc") then
+       let x = int (line.TrimStart("acc ".ToCharArray()))
+       Acc x 
+    else if line.Contains("jmp") then
+       let x = int (line.TrimStart("jmp ".ToCharArray()))
+       Jmp x 
+    else 
+       Nop 
+
+let parseLines (input: string[]): Cmd list =
+    input 
+      |> Array.map (fun s -> parseLine s) 
+      |> Array.toList
+
+[<Fact>]
+let ``Parse example file equal lines`` () =
+    let file = readInputfile "data/example.txt"
+    Assert.Equal(9, parseLines file |> List.length)
+
+[<Fact>]
+let ``Parse file equal lines`` () =
+    let file = readInputfile "data/input.txt"
+    Assert.Equal(file.Length, parseLines file |> List.length)
+
+[<Fact>]
+let ``Parse Nop`` () =
+    Assert.Equal( Nop, parseLine "nop -4")
+
+[<Fact>]
+let ``Parse Jmp 4`` () =
+    Assert.Equal( Jmp 4, parseLine "jmp +4")
+
+[<Fact>]
+let ``Parse Acc -3`` () =
+    Assert.Equal( Acc -3, parseLine "acc -3")
+
+[<Fact>]
+let ``Parse Jmp -3`` () =
+    Assert.Equal( Jmp -3, parseLine "jmp -3")
+
+[<Fact>]
+let ``Read file works`` () =
+    Assert.Contains("jmp +5", readInputfile "data/input.txt")
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0
