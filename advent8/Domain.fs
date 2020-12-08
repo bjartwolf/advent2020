a28d4690203cd52ada874000452ab754dbae552c
bjartwolf <bjartnes@gmail.com>
2020-12-08 12:18:41 +0100

    more

--- advent8/Domain.fs
+++ advent8/Domain.fs
@@ -8,19 +8,45 @@ type State = { PC : int;
                Acc: int;
                Visited: int list } 
 
-let initState = { PC = 0;
+let isCompleted (program: Program) (state: State) : bool =
+    let isFinished = state.PC + 1 >= program.Length 
+    let hasRunNextCommandBefore = List.contains (state.PC + 1) state.Visited
+    isFinished || hasRunNextCommandBefore
+
+let initState = { PC = -1;
                   Acc = 0;
-                  Visited = []}
+                  Visited = [] }
 
 let evaluate (program: Program) (state: State) : State =
-    let op = program.[state.PC]
-    let res = match op with 
-        | Acc a -> { state with Acc = state.Acc + a;
-                                PC = state.PC + 1}
-        | Jmp j -> { state with PC = state.PC + j }
-        | Nop -> { state with PC = state.PC + 1 }
-    
-    { res with Visited = state.PC :: state.Visited }
+    let pc = state.PC + 1
+    let op = program.[pc]
+    let res = 
+        match op with 
+            | Acc a -> { state with 
+                                Acc = state.Acc + a;
+                                PC = pc}
+            | Jmp j -> { state with 
+                                PC = state.PC + j } // it seems like jmp 1 equal nop
+            | Nop   -> { state with PC = pc }
+        
+    { res with Visited = pc :: state.Visited }
+
+[<Fact>]
+let ``Evaluate 1 acc and 1 jump steps`` () =
+    let program = [Acc 2; Jmp 2; Acc 4; Acc 13] 
+    let programEvaluator = evaluate program
+    let output = evaluate program initState 
+
+    Assert.Equal({ PC = 0;
+                   Acc = 2;
+                   Visited = [0] } , output )
+
+    let final = output |> programEvaluator |> programEvaluator
+
+    Assert.Equal({ PC = 3;
+                   Acc = 15;
+                   Visited = [3; 1; 0] } , final )
+    Assert.True(isCompleted program final)
 
 [<Fact>]
 let ``Evaluate 2 acc steps`` () =
@@ -28,13 +54,17 @@ let ``Evaluate 2 acc steps`` () =
     let programEvaluator = evaluate program
 
     let output = evaluate program initState 
-    Assert.Equal({ PC = 1;
+    Assert.Equal({ PC = 0;
                    Acc = 2;
                    Visited = [0] } , output )
 
-    Assert.Equal({ PC = 2;
+    Assert.False(isCompleted program output)
+
+    let nextState = programEvaluator output
+    Assert.Equal({ PC = 1;
                    Acc = 6;
-                   Visited = [1; 0] } , programEvaluator output )
+                   Visited = [1; 0] } , nextState)
+    Assert.True(isCompleted program nextState)
 
 [<Fact>]
 let ``Evaluate 2 nop steps`` () =
@@ -42,25 +72,27 @@ let ``Evaluate 2 nop steps`` () =
     let programEvaluator = evaluate program
 
     let output = evaluate program initState 
-    Assert.Equal({ PC = 1;
+    Assert.Equal({ PC = 0;
                    Acc = 0;
                    Visited = [0] } , output )
 
-    Assert.Equal({ PC = 2;
+    Assert.False(isCompleted program output)
+
+    let nextState = programEvaluator output 
+    Assert.Equal({ PC = 1;
                    Acc = 0;
-                   Visited = [1; 0] } , programEvaluator output )
+                   Visited = [1; 0] }, nextState)
+
+    Assert.True(isCompleted program nextState)
 
 [<Fact>]
 let ``Evaluate 1 nop step`` () =
     let program = [Nop] 
     let programEvaluator = evaluate program
 
-    let state = { PC = 0;
-                  Acc = 0;
-                  Visited = []}
-
-    let output = programEvaluator state 
-    Assert.Equal({ PC = 1;
+    let output = programEvaluator initState 
+    Assert.Equal({ PC = 0;
                    Acc = 0;
                    Visited = [0] } , output )
 
+    Assert.True(isCompleted program output)
