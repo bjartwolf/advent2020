f63bba86d871a2a9c3a21872052a652f9b4a0140
bjartwolf <bjartnes@gmail.com>
2020-12-08 11:11:40 +0100

    more

---
+++ advent8/Domain.fs
@@ -0,0 +1,66 @@
+﻿module Domain
+open Xunit
+
+type Cmd = Nop | Jmp of int | Acc of int
+type Program = Cmd list
+
+type State = { PC : int;
+               Acc: int;
+               Visited: int list } 
+
+let initState = { PC = 0;
+                  Acc = 0;
+                  Visited = []}
+
+let evaluate (program: Program) (state: State) : State =
+    let op = program.[state.PC]
+    let res = match op with 
+        | Acc a -> { state with Acc = state.Acc + a;
+                                PC = state.PC + 1}
+        | Jmp j -> { state with PC = state.PC + j }
+        | Nop -> { state with PC = state.PC + 1 }
+    
+    { res with Visited = state.PC :: state.Visited }
+
+[<Fact>]
+let ``Evaluate 2 acc steps`` () =
+    let program = [Acc 2; Acc 4] 
+    let programEvaluator = evaluate program
+
+    let output = evaluate program initState 
+    Assert.Equal({ PC = 1;
+                   Acc = 2;
+                   Visited = [0] } , output )
+
+    Assert.Equal({ PC = 2;
+                   Acc = 6;
+                   Visited = [1; 0] } , programEvaluator output )
+
+[<Fact>]
+let ``Evaluate 2 nop steps`` () =
+    let program = [Nop; Nop] 
+    let programEvaluator = evaluate program
+
+    let output = evaluate program initState 
+    Assert.Equal({ PC = 1;
+                   Acc = 0;
+                   Visited = [0] } , output )
+
+    Assert.Equal({ PC = 2;
+                   Acc = 0;
+                   Visited = [1; 0] } , programEvaluator output )
+
+[<Fact>]
+let ``Evaluate 1 nop step`` () =
+    let program = [Nop] 
+    let programEvaluator = evaluate program
+
+    let state = { PC = 0;
+                  Acc = 0;
+                  Visited = []}
+
+    let output = programEvaluator state 
+    Assert.Equal({ PC = 1;
+                   Acc = 0;
+                   Visited = [0] } , output )
+
---
+++ advent8/Parser.fs
@@ -0,0 +1,54 @@
+﻿module Parser
+open Domain
+open Xunit
+open System.IO
+
+let readInputfile inputfile : string[] =
+    File.ReadAllLines inputfile
+
+let parseLine (line: string) : Cmd =
+    if line.Contains("acc") then
+       let x = int (line.TrimStart("acc ".ToCharArray()))
+       Acc x 
+    else if line.Contains("jmp") then
+       let x = int (line.TrimStart("jmp ".ToCharArray()))
+       Jmp x 
+    else 
+       Nop 
+
+let parseLines (input: string[]): Cmd list =
+    input 
+      |> Array.map (fun s -> parseLine s) 
+      |> Array.toList
+
+[<Fact>]
+let ``Parse example file equal lines`` () =
+    let file = readInputfile "data/example.txt"
+    Assert.Equal(9, parseLines file |> List.length)
+
+[<Fact>]
+let ``Parse file equal lines`` () =
+    let file = readInputfile "data/input.txt"
+    Assert.Equal(file.Length, parseLines file |> List.length)
+
+[<Fact>]
+let ``Parse Nop`` () =
+    Assert.Equal( Nop, parseLine "nop -4")
+
+[<Fact>]
+let ``Parse Jmp 4`` () =
+    Assert.Equal( Jmp 4, parseLine "jmp +4")
+
+[<Fact>]
+let ``Parse Acc -3`` () =
+    Assert.Equal( Acc -3, parseLine "acc -3")
+
+[<Fact>]
+let ``Parse Jmp -3`` () =
+    Assert.Equal( Jmp -3, parseLine "jmp -3")
+
+[<Fact>]
+let ``Read file works`` () =
+    Assert.Contains("jmp +5", readInputfile "data/input.txt")
+
+
