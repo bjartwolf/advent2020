de4a2738db89bf9fa8d0af9dce38e8b9c5395f5b
bjartwolf <bjartnes@gmail.com>
2020-12-10 09:53:00 +0100

    init

--- advent10/Program.fs
+++ advent10/Program.fs
@@ -21,8 +21,8 @@ let pick (adapters: int list) (nextAdapters: int list) : int =
    // for each adapter must find the ones that can fit in one... exists one that fits
    let possibleNextAdapters = adapters |> List.map (fun a -> (a, findConnectors nextAdapters a))
    let possibleConnections = possibleNextAdapters 
-                             |> List.where ( fun (a,conns) -> not (List.isEmpty conns))
-                             |> List.map (fun (a, conns) -> a)
+                             |> List.where ( fun (_,conns) -> not (List.isEmpty conns))
+                             |> List.map (fun (a, _) -> a)
    if List.isEmpty possibleConnections then 
        -1
    else 
@@ -31,8 +31,7 @@ let pick (adapters: int list) (nextAdapters: int list) : int =
 let testdata = System.IO.File.ReadAllLines "data/foo.txt" |> Array.map (int) |> Array.toList
 
 let findDifferences (input: int list) (difference: int) = 
-// could assert on sort
-    input |> Seq.pairwise |> Seq.where (fun (a,b) -> b - a = difference) |> Seq.toList |> List.length 
+    input |> Seq.sort |> Seq.pairwise |> Seq.where (fun (a,b) -> b - a = difference) |> Seq.toList |> List.length 
 
 [<Fact>]
 let ``inputss`` () = 
@@ -52,6 +51,27 @@ let ``inputss`` () =
 
     let list = [1;3;5;7]
     Assert.Equal(3, findDifferences list 2)
+
+let rec createVisitedSequence (visited: int list) (input: int list) (start: int) : int list =
+    // starter med input
+    // legger på til -1... da henter man ratingen og legger til den og
+    let possibleConnectors = findConnectors input start 
+    let firstConnector = pick possibleConnectors input 
+    if firstConnector = -1 then  
+        // little sort of bug where it should probably have returned 19
+        let connectTo = possibleConnectors |> List.head
+        rating [connectTo ]:: connectTo :: start :: visited 
+    else 
+        createVisitedSequence (start :: visited) input firstConnector
+
+[<Fact>]
+let ``visitTestfile`` () = 
+    let connectors = createVisitedSequence [] testdata chargingOutlet
+    let ones = findDifferences connectors 1
+    let threes = findDifferences connectors 3
+    Assert.Equal(7, ones)
+    Assert.Equal(5, threes)
+
 
 [<Fact>]
 let ``testdata adapters only 4 can connect to 1`` () = 
@@ -129,7 +149,6 @@ let ``testdata adapters only 4 can connect to 1`` () =
                       List.head elevens;
                       rating elevens]
     let ones = findDifferences connectors 1
-    let twos = findDifferences connectors 2
     let threes = findDifferences connectors 3
     Assert.Equal(7, ones)
     Assert.Equal(5, threes)
