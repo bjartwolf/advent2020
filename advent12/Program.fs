c5e133925cd616c353dd42cdd0e8e5b6eec925a5
bjartwolf <bjartnes@gmail.com>
2020-12-12 00:35:32 +0100

    init

---
+++ advent12/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Xunit
+
+[<Fact>]
+let ``foo`` () =
+    let foo = IO.File.ReadAllLines "data/test2.txt"
+    Assert.Equal(1, foo |> Array.length)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
