28641bf94e58ce8ca064910346f55f23b2966e0e
bjartwolf <bjartnes@gmail.com>
2020-12-17 13:30:30 +0100

    works, should be refactoed a bit but

--- advent17/Program.fs
+++ advent17/Program.fs
@@ -1,10 +1,10 @@
 ﻿open System
 open Xunit
 
-type Point = int * int * int
+type Point = int * int * int * int
 type Points = Set<Point> 
 
-let add ((x1,y1,z1): Point) ((x2,y2,z2) : Point) : Point = (x1+x2, y1+y2, z1+z2)
+let add ((x1,y1,z1,t1): Point) ((x2,y2,z2,t2) : Point) : Point = (x1+x2, y1+y2, z1+z2, t1 + t2)
 
 let neighbors (p:Point) : Points =  
     let diff = [-1;0;1]; 
@@ -12,8 +12,9 @@ let neighbors (p:Point) : Points =
         for x in diff do
             for y in diff do
                 for z in diff do
-                    if (x,y,z) <> (0,0,0) then 
-                        yield (add p (x,y,z))
+                    for t in diff do
+                        if (x,y,z,t) <> (0,0,0,0) then 
+                            yield (add p (x,y,z,t))
     } |> Set.ofSeq
 
 // returns a set of possible cells that might be alive
@@ -42,18 +43,18 @@ let inputData : Points =
             i <- i + 1 
             for char in line do
                 j <- j + 1
-                if (char = '#') then yield (i,j,0)
+                if (char = '#') then yield (i,j,0,0)
     }
     points |> Set.ofSeq
     
 
 [<Fact>]
 let ``find alive neighbors`` () =
-    let n = countAliveNeighbors (10,1,2) (Set.empty)
+    let n = countAliveNeighbors (10,1,2,0) (Set.empty)
     Assert.Equal(0, n)
 
 let countPlane (s: Points) (plane: int) : int =
-    s |> Set.filter (fun (_,_,p) -> p = plane) |> Set.count
+    s |> Set.filter (fun (_,_,p,0) -> p = plane) |> Set.count
 
 [<Fact>]
 let ``input 6 rounds is 120 alive`` () =
@@ -68,7 +69,7 @@ let ``input 6 rounds is 120 alive`` () =
 
 [<Fact>]
 let ``test 6 rounds is 120 alive`` () =
-    let gen0: Points = Set.ofList[ (0,1,0); (1,2,0); (2,0,0); (2,1,0); (2,2,0) ] 
+    let gen0: Points = Set.ofList[ (0,1,0,0); (1,2,0,0); (2,0,0,0); (2,1,0,0); (2,2,0,0) ] 
     let gen1 = nextGeneration gen0
     let gen2 = nextGeneration gen1
     let gen3 = nextGeneration gen2
@@ -79,7 +80,7 @@ let ``test 6 rounds is 120 alive`` () =
 
 [<Fact>]
 let ``test1`` () =
-    let gen0: Points = Set.ofList[ (0,1,0); (1,2,0); (2,0,0); (2,1,0); (2,2,0) ] 
+    let gen0: Points = Set.ofList[ (0,1,0,0); (1,2,0,0); (2,0,0,0); (2,1,0,0); (2,2,0,0) ] 
     let gen1 = nextGeneration gen0 
     Assert.Equal(5, countPlane gen1 0) 
     Assert.Equal(3, countPlane gen1 1) 
@@ -98,16 +99,23 @@ let ``test1`` () =
 
 [<Fact>]
 let ``find alive neighbors with itself in the set`` () =
-    let s = Set.ofList[ (10,1,2); (10,1,3); (10,1,4)]
-    let n = countAliveNeighbors (10,1,2) s 
+    let s = Set.ofList[ (10,1,2,0); (10,1,3,0); (10,1,4,0)]
+    let n = countAliveNeighbors (10,1,2,0) s 
     Assert.Equal(1, n) 
 
 [<Fact>]
 let ``find neighbors`` () =
-    let n = neighbors (10,1,2)
+    let n = neighbors (10,1,2,0)
     Assert.Equal(26, n |> Seq.length)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let gen0: Points = inputData 
+    let gen1 = nextGeneration gen0
+    let gen2 = nextGeneration gen1
+    let gen3 = nextGeneration gen2
+    let gen4 = nextGeneration gen3
+    let gen5 = nextGeneration gen4
+    let gen6 = nextGeneration gen5
+    printfn "Answer if %A" (gen6 |> Set.count)
     0 // return an integer exit code
