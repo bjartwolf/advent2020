d5e9c102d67e85eb0bca83bc0a15b3b1f23a5196
bjartwolf <bjartnes@gmail.com>
2020-12-18 14:00:20 +0100

    modular addition theorem

--- advent18/Program.fs
+++ advent18/Program.fs
@@ -12,16 +12,6 @@ let rec calc (es: Expressions) : int64 =
         | Nr x :: Mult :: Nr y :: rest ->  calc ( Nr (x * y) :: rest ) 
         | [Nr x] -> x 
 
-        (*
-[<Fact>]
-let ``Calcuations works`` () =
-    Assert.Equal(71,calc [Nr 1 ; Plus; Nr 2; Mult; Nr 3 ; Plus ; Nr 4 ; Mult ; Nr 5 ; Plus ; Nr 6] )
-    Assert.Equal(51, calc [Nr 1; Plus ; Exp [Nr 2 ; Mult; Nr 3]; Plus ; Exp [Nr 4 ; Mult ; Exp [ Nr 5; Plus; Nr 6]]])
-    Assert.Equal(26, calc [Nr 2; Mult; Nr 3; Plus; Exp [Nr 4; Mult; Nr 5]]) 
-    Assert.Equal(437, calc [Nr 5; Plus ; Exp [Nr 8 ; Mult ; Nr 3; Plus; Nr 9; Plus ; Nr 3; Mult; Nr 4; Mult; Nr 3]])
-    Assert.Equal(12240, calc [Nr 5; Mult; Nr 9; Mult; Exp[Nr 7 ; Mult; Nr 3; Mult; Nr 3; Plus; Nr 9; Mult; Nr 3; Plus; Exp [Nr 8; Plus; Nr 6; Mult; Nr 4 ]]])
-    Assert.Equal(13632, calc [Exp[Exp[ Nr 2; Plus; Nr 4; Mult; Nr 9]; Mult; Exp [Nr 6; Plus; Nr 9; Mult; Nr 8; Plus; Nr 6]; Plus; Nr 6]; Plus; Nr 2; Plus; Nr 4; Mult; Nr 2])
-*)
 let findClosingParen (s:string) : int = 
     let mutable count = 1
     let mutable i = 0
@@ -35,15 +25,6 @@ let findClosingParen (s:string) : int =
         i <- i + 1
     i - 1
 
-    (*
-[<Fact>]
-let ``find close works`` () =
-    Assert.Equal(2, findClosingParen "())")
-    Assert.Equal(2, findClosingParen "())()")
-    Assert.Equal(4, findClosingParen "(()))()")
-    Assert.Equal(8, findClosingParen "((()))())(())")
-    Assert.Equal(0, findClosingParen ")")
-*)
 let rec parse (s1: string) : Expressions =
     let s = s1.TrimStart()
     if s = "" then []
@@ -65,22 +46,16 @@ let rec parse (s1: string) : Expressions =
             let s'' = s.Substring(indexOfMult + 1)
             Nr (int64 s') ::  Mult :: parse s''
 
-            (*
-[<Fact>]
-let ``Parser works`` () =
-   let answer2 = ([Exp [Nr 1; Plus; Exp [ Nr 3; Mult; Nr 5]]] = (parse "(1 + (3 * 5))"))
-   Assert.True(answer2)
-   let answer1 = ([Exp [Nr 1; Plus; Nr 3]] = (parse "(1 + 3)"))
-   Assert.True(answer1)
-   let answer3 = ([Nr 1; Plus; Nr 3; Plus; Nr 5] = (parse "1 + 3 + 5"))
-   Assert.True(answer3)
-   let answer4 = ([Nr 1; Plus; Nr 3; Plus; Nr 5; Mult; Nr 6] = (parse "1 + 3 + 5 * 6"))
-   Assert.True(answer4)
-*)
 [<Fact>]
 let ``Calc on strings works`` () =
     Assert.Equal(13632L, calc (parse "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
     Assert.Equal(71L, calc (parse "1 + 2 * 3 + 4 * 5 + 6"))
+
+[<Fact>]
+let ``Nr 1 works`` () =
+    let file = IO.File.ReadAllLines "data/input.txt"
+    let res = file |> Array.map (fun l ->  calc (parse l)) |> Array.map (int64) |> Array.sum
+    Assert.Equal(280014646144L, res)
 
 [<EntryPoint>]
 let main argv =
