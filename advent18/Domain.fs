388b635e039c26a140cd528b8c3b6041ab6d29a5
bjartwolf <bjartnes@gmail.com>
2020-12-18 16:36:39 +0100

    giving up

---
+++ advent18/Domain.fs
@@ -0,0 +1,4 @@
+﻿module Domain
+
+type Exp = Plus | Mult | Nr of int64 | Exp of Exp list 
+type Expressions = Exp list 
--- advent18/Program.fs
+++ advent18/Program.fs
@@ -1,8 +1,7 @@
 ﻿open System
 open Xunit
-
-type Exp = Plus | Mult | Nr of int64 | Exp of Exp list 
-type Expressions = Exp list
+open Parser
+open Domain
 
 let rec calc (es: Expressions) : int64 = 
     match es with 
@@ -12,40 +11,6 @@ let rec calc (es: Expressions) : int64 =
         | Nr x :: Mult :: Nr y :: rest ->  calc ( Nr (x * y) :: rest ) 
         | [Nr x] -> x 
         | [Exp x] -> calc x 
-
-let findClosingParen (s:string) : int = 
-    let mutable count = 1
-    let mutable i = 0
-    while (count > 0) do
-        if s.[i] = '(' then
-           count <- count + 1 
-        else if s.[i] = ')' then
-            count <- count - 1
-        else
-            ()
-        i <- i + 1
-    i - 1
-
-let rec parse (s1: string) : Expressions =
-    let s = s1.TrimStart()
-    if s = "" then []
-    else if s.[0] = '*' then Mult :: parse (s.Substring(1))
-    else if s.[0] = '+' then Plus :: parse (s.Substring(1))
-    else if (s.[0] = '(') then 
-        let closeParen = findClosingParen(s.Substring(1)) 
-        Exp (parse (s.Substring(1,closeParen))) :: parse (s.Substring(closeParen + 2))
-    else 
-        let indexOfPlus = s.IndexOf('+')
-        let indexOfMult = s.IndexOf('*')
-        if (indexOfMult = -1 && indexOfPlus = -1) then [Nr (int64 s)] 
-        else if (indexOfPlus <> -1 && (indexOfMult = -1 || indexOfPlus < indexOfMult )) then 
-            let s' = s.Substring(0,indexOfPlus)
-            let s'' = s.Substring(indexOfPlus + 1)
-            Nr (int64 s') ::  Plus :: parse s''
-        else
-            let s' = s.Substring(0,indexOfMult)
-            let s'' = s.Substring(indexOfMult + 1)
-            Nr (int64 s') ::  Mult :: parse s''
 
 
 let rec addParens (exps: Expressions) : Expressions =
@@ -76,9 +41,6 @@ let ``Add parens works`` () =
    let exp2 = addParens (parse s2)
    let eq2= exp2 = [Exp [Nr 9L; Plus; Nr 4L]; Mult; Exp [Nr 3L; Plus; Nr 9L]]
    Assert.True(eq2)
-
-   
-
 
 [<Fact>]
 let ``Calc on strings works`` () =
