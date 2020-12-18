8c5cc92ef00cd28f794cbd96147c67e7ec380987
bjartwolf <bjartnes@gmail.com>
2020-12-18 09:13:18 +0100

    this is a bit crazy tdd

--- advent18/Program.fs
+++ advent18/Program.fs
@@ -11,10 +11,9 @@ let rec calc (es: Expressions) : int =
         | Nr x :: Plus :: Nr y :: rest ->  calc ( Nr (x + y) :: rest)
         | Nr x :: Mult :: Nr y :: rest ->  calc ( Nr (x * y) :: rest ) 
         | [Nr x] -> x 
-        | [Exp x] -> calc x 
 
 [<Fact>]
-let ``foo`` () =
+let ``Calcuations works`` () =
     Assert.Equal(71,calc [Nr 1 ; Plus; Nr 2; Mult; Nr 3 ; Plus ; Nr 4 ; Mult ; Nr 5 ; Plus ; Nr 6] )
     Assert.Equal(51, calc [Nr 1; Plus ; Exp [Nr 2 ; Mult; Nr 3]; Plus ; Exp [Nr 4 ; Mult ; Exp [ Nr 5; Plus; Nr 6]]])
     Assert.Equal(26, calc [Nr 2; Mult; Nr 3; Plus; Exp [Nr 4; Mult; Nr 5]]) 
@@ -22,6 +21,21 @@ let ``foo`` () =
     Assert.Equal(12240, calc [Nr 5; Mult; Nr 9; Mult; Exp[Nr 7 ; Mult; Nr 3; Mult; Nr 3; Plus; Nr 9; Mult; Nr 3; Plus; Exp [Nr 8; Plus; Nr 6; Mult; Nr 4 ]]])
     Assert.Equal(13632, calc [Exp[Exp[ Nr 2; Plus; Nr 4; Mult; Nr 9]; Mult; Exp [Nr 6; Plus; Nr 9; Mult; Nr 8; Plus; Nr 6]; Plus; Nr 6]; Plus; Nr 2; Plus; Nr 4; Mult; Nr 2])
 
+let rec parse (s: string) : Expressions =
+    let indexOfPlus = s.IndexOf('+')
+    let indexOfMult = s.IndexOf('*')
+    if (indexOfMult = -1 && indexOfPlus = -1) then [Nr (int s)] 
+    else let s' = s.Split("+") 
+         printfn "%A" s'
+         [Nr (int s'.[0]) ; Plus ; Nr (int s'.[1])]
+
+[<Fact>]
+let ``Parser works`` () =
+   let answer2 = ([Nr 1; Plus; Nr 3] = (parse "1 + 3"))
+   Assert.True(answer2)
+   let answer = [Nr 1] = (parse "1")
+   Assert.True(answer)
+ 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
